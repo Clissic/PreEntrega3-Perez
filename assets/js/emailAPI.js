b85a416cec13176dedf224d1ac2ce61c2d706fde
Joaquin PEREZ <joaquin.perez.coria@gmail.com>
2023-02-14 12:57:40 -0300

    Arreglando errores

--- assets/js/emailAPI.js
+++ assets/js/emailAPI.js
@@ -3,48 +3,21 @@ const emailFrom = document.getElementById("inputEmailContacto").textContent;
 const emailBody = document.getElementById("comentarioContacto").textContent;
 const btnEnviarEmail = document.getElementById("btnEnviarEmail");
 
-
-//APIKey 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
-
 const sendEmail = async () => {
-  const details = {
-    to: "joaquin.perez.coria@gmail.com",
-    from: "joaquin.perez.coria@gmail.com",
-    subject: "Comentario de " + `${emailAuthor}`,
-    text: `${emailBody}`,
-  };
-
-  const response = await fetch("https://api.sendgrid.com/v3/mail/send", {
-    method: "POST",
-    headers: {
-      Authorization: `Bearer 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`,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      personalizations: [
-        {
-          to: [{ email: details.to }],
-          subject: details.subject,
-        },
-      ],
-      from: { email: details.from },
-      content: [
-        {
-          type: "text/plain",
-          value: details.text,
-        },
-      ],
-    }),
-  });
-
-  if (response.ok) {
-    console.log("Correo electrónico enviado correctamente");
-  } else {
-    console.error(
-      "Error al enviar el correo electrónico:",
-      response.statusText
-    );
-  }
+	const options = {
+		method: 'POST',
+		headers: {
+			'content-type': 'application/json',
+			'X-RapidAPI-Key': '',
+			'X-RapidAPI-Host': 'rapidprod-sendgrid-v1.p.rapidapi.com'
+		},
+		body: '{"personalizations":[{"to":[{"email":"john@example.com"}],"subject":"Hello, World!"}],"from":{"email":"from_address@example.com"},"content":[{"type":"text/plain","value":"Hello, World!"}]}'
+	};
+	
+	fetch('https://rapidprod-sendgrid-v1.p.rapidapi.com/mail/send', options)
+		.then(response => response.json())
+		.then(response => console.log(response))
+		.catch(err => console.error(err));
   
 console.log(emailAuthor, emailBody, emailFrom);
 };
